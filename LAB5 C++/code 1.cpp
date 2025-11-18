f6a44b1764e2cb82a568ce8392cc1d2289c7caee
Hai_Tay_Wakai <sslikoolik197@gmail.com>
2025-11-18 14:34:59 +0700

    Add files via upload

---
+++ LAB5 C++/code 1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int row,i,j;
+    char sign;
+    
+    cin >> row;
+    cin >> sign;
+    
+    for (i = row; i >= 1; i--) {      
+        for (j = 1; j <= i; j++) {
+            cout << sign;
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}

035e078060577188f56403f07b87d2b7128db26a
Hai_Tay_Wakai <sslikoolik197@gmail.com>
2025-11-18 14:54:59 +0700

    Add files via upload

---
+++ LAB5 C++/code333.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,i;
+	cin >> n;
+	  for (i=1;i<=n;i++){
+	  	
+	  	if(i%2==1){
+	  		cout <<0<<"";
+		  }else{
+		  	cout<<1<<"";
+		  }
+	  }
+return 0;
+}
